016599b5198d65a2de3cddf556a970b57803a70a
BT_3W2_26 <Khaledo@nycstudents.net>
2024-11-18 13:21:20 -0500

    trtrhtrsethtrshtrs

--- StringLoops.java
+++ StringLoops.java
@@ -4,34 +4,44 @@ public class StringLoops
     public StringLoops() { }
 
     /* Returns the number of times "character" appears in "searchString"
+
        This should be case sensitive!
 
        Examples:
        - if character = "a" and searchString = "Apples and bananas",
-         this method returns 4
+         this method returns 4 (it finds BOTH "A" and "a")
        - if character = "A" and searchString = "Apples and bananas",
-         this method returns 1
+         this method returns 1 (it finds BOTH "A" and "a")
        - if character = "!" and searchString = "Hello! Nice day!",
          this method returns 2
       */
     public int countCharacters(String character, String searchString)
     {
-        /* to be implemented */
+        /* if you finished this in the last lesson, start with “reverseString2” */
+        /* if you did not, complete this now! */
     }
 
-    /* Returns the original string reversed
+
+    public String reverseString(String origString)
+    {
+        String print = origString;
+        String newStr = "";
+        for(int i = 0; i < print.length(); i++)
+        {
+            String letter = (print.substring(print.length() - i -1, print.length() - i));
+
+            System.out.println(letter);
+    }
+
+// --- FIVE NEW METHODS TO ADD & IMPLEMENT ARE BELOW ---
+
+    /* Returns the original string reversed -- SECOND IMPLEMENTATION
 
      Examples:
      - if origString = "hello!" this method returns "!olleh"
      - if origString = "Apples and bananas" this method returns "sananab dna selppA"
     */
-    public String reverseString(String origString)
+    public String reverseString2(String origString)
     {
-        String rev = "";
-        for(int i = origString.length() - 1; i >= 0; i--){
-            rev = rev + origString.charAt(i);
-        }
-        return rev;
-        /* to be implemented */
+
     }
-}
--- U4L34.java
+++ U4L34.java
@@ -1,10 +1,12 @@
 public class U4L34 {
     public static void main (String[] args){
         String print = "Print Me!";
-
+        String newStr = "";
         for(int i = 0; i < print.length(); i++)
         {
-            System.out.println(print.substring(i, i + 1));
+           String letter = (print.substring(print.length() - i -1, print.length() - i));
+
+            System.out.println(letter);
         }
 
     }
